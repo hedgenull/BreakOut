be1518864b55beff71d064b48c9f39c15a1d65f2
Hedge <hedgefleming@gmail.com>
2022-03-18 13:48:16 -0400

    Started work on breakout.py

---
+++ breakout.py
@@ -0,0 +1,18 @@
+#================================================================================>
+# FILE NAME:  breakout.py
+#
+# PURPOSE:
+# Main game file for BreakOut game.
+#
+# CREATED DATE: 2022-03-18
+# AUTHOR:       Greyson Smith (mathopotamus@bearcreek.family)
+#================================================================================>
+
+
+import pygame
+
+
+class BreakOut:
+    """Base class for BreakOut game."""
+    def __init__(self):
+        """Initialize BreakOut assets and objects."""
